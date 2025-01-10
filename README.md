5c599f29958ff8c2dbf9126893cc4c5dd6caed1c
zxp <601522843@qq.com>
2025-01-10 15:56:09 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DataSimulation
+
 用于生成模拟数据放入kafka供消费者消费
---
+++ src/test/java/com/dust/datasimulation/DatasimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dust.datasimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasimulationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
